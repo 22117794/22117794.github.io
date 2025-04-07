2c2e6118ba4c0bcdf34ee479d52173d60cd9015a
22117794 <22117794@cambria.ac.uk>
2025-04-07 13:57:12 +0100

    Add files via upload

---
+++ U6_A2/Javascript1.js
@@ -0,0 +1,9 @@
+var Digital=new Date()
+var hours=Digital.getHours()
+if(hours>=0&&hours<=12)
+document.write('Good Morning, Welcome to the Celtec Games Website')
+else if(hours>12&&hours<=17)
+document.write('Good Afternoon, Welcome to the Celtec Games Website')
+else
+document.write('Good Evening, Welcome to the Celtec Games Website')
+
